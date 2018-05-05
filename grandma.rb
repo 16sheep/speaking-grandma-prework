f3f4faa844774aeb127f1c558acbccea45a04b7c
marjutubli@gmail.com <marjutubli@gmail.com>
2018-05-05 16:11:27 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,10 +1,10 @@
 # Write a speak_to_grandma method.
 
 def speak_to_grandma(phrase)
-  love = "I LOVE YOU TOO PUMPKIN!"
+  love = "I LOVE YOU GRANDMA!"
   uppercase_phrase = phrase.upcase
   if phrase == love
-    love
+    "I LOVE YOU TOO PUMPKIN!"
   
   elsif uppercase_phrase == phrase
     "NO, NOT SINCE 1938!"
