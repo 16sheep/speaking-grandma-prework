76fbfadd539a758b7ff44d3fdf1f6bade4295f44
marjutubli@gmail.com <marjutubli@gmail.com>
2018-05-05 16:06:36 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -4,9 +4,9 @@ def speak_to_grandma(phrase)
   love = "I LOVE YOU GRANDMA!"
   uppercase_phrase = phrase.upcase
 if phrase == love
-    return love
+  return love
 elsif uppercase_phrase == phrase
-    return "NO, NOT SINCE 1938!"
+  return "NO, NOT SINCE 1938!"
 else
-    return "HUH?! SPEAK UP, SONNY!"
+  return "HUH?! SPEAK UP, SONNY!"
 end
